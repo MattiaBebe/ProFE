089c42efc4539f099f893ac160461a193c515518
MattiaBebe <mattia.bertinelli@cypag-group.com>
2021-11-15 15:22:42 +0100

    aggiunta pulsanti selezione

---
+++ src/components/PulsantiSelezione.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PulsantiSelezione = (props) => {
+    
+    return(
+        <button type="button"> BOTTONE 1 </button>
+    );
+
+}
+
+export default PulsantiSelezione;
